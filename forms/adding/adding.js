71d6f9932ead78da88b37c0c430f8cf24a814833
asakurasaki <90635229+asakurasaki@users.noreply.github.com>
2021-09-20 15:50:14 -0500

    second getting number from user n muliply

--- forms/adding/adding.js
+++ forms/adding/adding.js
@@ -1,3 +1,9 @@
 let num1 = 10
 let nun2 = 20
 console.log(`The sum is ${num1 + num2)`)
+
+let newNum = Number(prompt("enter num"))
+console.log(`if you multiply the second number with the new number you get ${num2 * newNum}`)
+
+let newNum2 = Number(prompt("enter num"))
+console.log(`if you multiply the second number with the new number you get ${num2 * newNum2}`)
